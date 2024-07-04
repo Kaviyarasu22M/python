28c5b0d45d523d3482d2a8e8f522b120f6f9a7bf
Kaviyarasu22M <mkaviyarasu81@gmail.com>
2024-07-04 20:43:43 +0530

    Task4

---
+++ Task4_userloop.py
@@ -0,0 +1,8 @@
+n=int(input("enter the how much your number RANGE:"))
+a=int(input("enter the ommit number1:"))
+b=int(input("enter the ommit number2"))
+for i in range(n+1):
+    if i == a or i==b:
+        continue
+    else:
+        print(i)
